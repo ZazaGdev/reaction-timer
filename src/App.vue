ea1894404ba0ac39f320e62f640ca0c458ec6823
ZazaGdev <Gejadze250@gmail.com>
2023-04-26 11:42:19 +0400

    Initiated game's initial CSS and HTML

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
-    <h1>Reaction Timer</h1>
+    <div class="flex justify-center items-center flex-col">
+        <h1 class="text-center text-5xl mt-12 text-indigo-800">Reaction Timer</h1>
+        <button
+            @click="start"
+            :disabled="isPlaying"
+            class="bg-indigo-500 text-white mt-4 rounded-xl py-2 px-7 text-lg"
+            :class="{ 'opacity-20': isPlaying === true }"
+        >
+            Play
+        </button>
+    </div>
+    <div class="blockWrapper border-double border-4 border-indigo-600 m-12 h-3/4">
+        <Block v-if="isPlaying" :delay="delay" />
+    </div>
 </template>
 
 <script>
+import Block from './components/Block.vue'
+
 export default {
     name: 'App',
-    components: {},
+    components: { Block },
+    data() {
+        return {
+            isPlaying: false,
+            delay: null,
+        }
+    },
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 5000
+            this.isPlaying = true
+            console.log(this.delay)
+        },
+    },
 }
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-48 h-48 bg-cyan-500 flex items-center justify-center text-4xl text-white">
+        <p>Here</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+}
+</script>
+
+<style></style>
