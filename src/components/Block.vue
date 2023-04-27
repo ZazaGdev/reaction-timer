036400a6295bc0f71e127fe6c01cbc356aee10ae
ZazaGdev <Gejadze250@gmail.com>
2023-04-27 12:14:48 +0400

    Added random positioning to the target block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,23 +1,43 @@
 <template>
-    <div @click="stopTimer" v-if="showBlock" class="w-48 h-48 bg-cyan-500 flex items-center justify-center text-4xl text-white">
+    <div
+        @click="stopTimer"
+        v-if="showBlock"
+        ref="blockElement"
+        class="absolute w-1/6 h-1/6 bg-cyan-500 flex items-center justify-center text-4xl text-white"
+        :style="{ left: blockLeft + 'px', top: blockTop + 'px' }"
+    >
         <p>Here</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['delay'],
+    props: ['delay', 'wrapperWidth', 'wrapperHeight'],
     data() {
         return {
             showBlock: false,
             timer: null,
             reactionTime: 0,
+            blockWidth: null,
+            blockHeight: null,
+            blockLeft: null,
+            blockTop: null,
         }
     },
     mounted() {
         setTimeout(() => {
             this.showBlock = true
             this.startTimer()
+
+            this.$nextTick(() => {
+                const targetElement = this.$refs.blockElement
+                const targetRect = targetElement.getBoundingClientRect()
+                this.blockWidth = targetRect.width
+                this.blockHeight = targetRect.height
+                console.log(this.wrapperHeight)
+                console.log(this.blockWidth, this.blockHeight)
+                this.calculatePosition()
+            })
         }, this.delay)
     },
     methods: {
@@ -28,8 +48,11 @@ export default {
         },
         stopTimer() {
             clearInterval(this.timer)
-            console.log(this.reactionTime)
             this.$emit('end', this.reactionTime)
+        },
+        calculatePosition() {
+            this.blockTop = Math.floor(Math.random() * (this.wrapperHeight - this.blockHeight)) + 1
+            this.blockLeft = Math.floor(Math.random() * (this.wrapperWidth - this.blockWidth)) + 1
         },
     },
 }
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,29 @@
 <template>
-    <p>Reaction Time {{ score }} ms</p>
+    <div class="text-center mt-5">
+        <p class="text-xl">
+            Reaction Time <span class="text-3xl">{{ score }}</span> ms
+        </p>
+        <p class="text-lg">Rank - {{ rank }}</p>
+    </div>
 </template>
 
 <script>
 export default {
     props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Extraordinary'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid'
+        } else {
+            this.rank = 'Slow'
+        }
+    },
 }
 </script>
 
